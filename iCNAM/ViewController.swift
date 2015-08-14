d97a6dbc16ce636f3205f494ce8c34a7a4ae1e5d
Corey Edwards <cedwardsmedia@gmail.com>
2015-08-13 22:43:46 -0500

    Initial Commit

---
+++ iCNAM/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iCNAM
+//
+//  Created by Corey Edwards on 8/13/15.
+//  Copyright (c) 2015 Corey Edwards. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
